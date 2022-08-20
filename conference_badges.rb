fc11459f7e2bb160e60b75a8798c3861cb006c14
Evans Rutto <robbyevans001@gmail.com>
2022-08-20 20:52:59 +0300

    commit tree 4032d67971930b5a520923dbbc414d234dc1dcf9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name) 
+  return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(array)
+  list = []
+  array.each {|name| list<<("Hello, my name is #{name}.")} 
+  return list
+  
+end        
+def assign_rooms(array)
+  list = []
+  array.each_with_index {|name,index| list<<("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+  return list
+end
+
+def printer(array)
+  batch_badge_creator(array).each {|names| puts names}
+  assign_rooms(array).each {|names| puts names}
+
+end    
+printer(["Arel", "Carol"])
+
+
